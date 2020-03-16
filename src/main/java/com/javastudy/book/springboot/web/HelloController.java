5f2e50097ff3ed0371e4aa2fca74abc073d992f3
yjs <yjs2952@naver.com>
2020-03-16 23:57:21 +0900

    dto 테스트 작성

--- src/main/java/com/javastudy/book/springboot/web/HelloController.java
+++ src/main/java/com/javastudy/book/springboot/web/HelloController.java
@@ -1,13 +1,20 @@
 package com.javastudy.book.springboot.web;
 
+import com.javastudy.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
