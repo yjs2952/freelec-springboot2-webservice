4c09ba6f661b1f2e706f357d6b77edc8d9afcf87
yjs <yjs2952@naver.com>
2020-03-18 20:08:11 +0900

    mustache 화면 조회

--- src/main/java/com/javastudy/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/javastudy/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.javastudy.book.springboot.service.posts;
 
 import com.javastudy.book.springboot.domain.posts.Posts;
 import com.javastudy.book.springboot.domain.posts.PostsRepository;
+import com.javastudy.book.springboot.web.dto.PostsListResponseDto;
 import com.javastudy.book.springboot.web.dto.PostsResponseDto;
 import com.javastudy.book.springboot.web.dto.PostsSaveRequestDto;
 import com.javastudy.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -27,8 +31,17 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id : " + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc()
+                .stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
