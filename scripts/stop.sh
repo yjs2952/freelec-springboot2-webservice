236ff70e0c4c9680584bc4c8828356577b40ed08
yjs <yjs2952@naver.com>
2020-04-27 19:41:10 +0900

    스크립트 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -11,10 +11,9 @@ IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -15 $IDLE_PID"
-    kill -15 ${IDLE_PID}
-    sleep 5
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep 5
 fi
-
